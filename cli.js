d90ebd6e5f209d3949596b00ef14b93e017096bc
hasezoey <10911626+hasezoey@users.noreply.github.com>
2018-03-06 20:16:50 +0100

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 var dir = require('./del.js').dir;
 console.log('Starting');
-dir(process.cwd(), () => { // first start (directory where executet)
+dir(process.cwd(), (dc) => { // first start (directory where executet)
     var path = process.cwd().grey;
     console.log(`Fully scanned "${path}"`.green);
-    console.log(`Deletet ${deletet_count.toString().magenta} directoris`);
+    console.log(typeof dc);
+    console.log(`Deletet ${dc.toString().magenta} directoris`);
 });
